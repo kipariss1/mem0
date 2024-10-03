866cd13c729716bf7b7fdfd5e202154bbfc60886
kipariss1 <kpassudin@gmail.com>
2024-10-03 23:35:18 +0200

    trying mem0 out

---
+++ mem0/test.py
@@ -0,0 +1,32 @@
+from mem0.memory.main import Memory
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+m = Memory()
+
+# 1. Add: Store a memory from any unstructured text
+result = m.add("I am working on improving my tennis skills. Suggest some online courses.", user_id="alice", metadata={"category": "hobbies"})
+
+# Created memory --> 'Improving her tennis skills.' and 'Looking for online suggestions.'
+
+# 2. Update: update the memory
+result = m.update(memory_id='<memory_id_1>', data="Likes to play tennis on weekends")
+
+# Updated memory --> 'Likes to play tennis on weekends.' and 'Looking for online suggestions.'
+
+# 3. Search: search related memories
+related_memories = m.search(query="What are Alice's hobbies?", user_id="alice")
+
+# Retrieved memory --> 'Likes to play tennis on weekends'
+
+# 4. Get all memories
+all_memories = m.get_all()
+memory_id = all_memories["memories"][0] ["id"] # get a memory_id
+
+# All memory items --> 'Likes to play tennis on weekends.' and 'Looking for online suggestions.'
+
+# 5. Get memory history for a particular memory_id
+history = m.history(memory_id='<memory_id_1>')
+
+# Logs corresponding to memory_id_1 --> {'prev_value': 'Working on improving tennis skills and interested in online courses for tennis.', 'new_value': 'Likes to play tennis on weekends' }
