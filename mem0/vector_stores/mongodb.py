00499685c1405922e3b7cb371e8b4b0344f49daf
kipariss1 <kpassudin@gmail.com>
2024-10-04 20:08:39 +0200

    first draft

---
+++ mem0/vector_stores/mongodb.py
@@ -0,0 +1,7 @@
+from mem0.vector_stores.base import VectorStoreBase
+
+# TODO: check here if pymongo installed and try to import it, if not - install
+
+
+class MongoDB_Atlas(VectorStoreBase):
+    pass
