6c406722d9c93df5e60e0185a998185bcbddc305
kipariss1 <kpassudin@gmail.com>
2024-10-28 23:33:38 +0100

    search almost works

--- mem0/configs/vector_stores/mongodb_atlas.py
+++ mem0/configs/vector_stores/mongodb_atlas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field, model_validator
-from typing import ClassVar, Optional, Dict, Any
+from pydantic import BaseModel, Field
+from typing import ClassVar, Optional, Literal
 
 
 class MongoDBAtlassConfig(BaseModel):
@@ -12,3 +12,6 @@ class MongoDBAtlassConfig(BaseModel):
     uri: str = Field(None, description="uri string to connect to MongoDB Atlass")
     database_name: Optional[str] = Field("mem0_db", description="name of the database in MongoDB Atlass")
     vector_field: Optional[str] = Field("vector_embedding", description="Path to the vector field in your documents")
+    similarity: Literal[
+        'euclidian', 'cosine', 'dotProduct'
+        ] = Field('cosine', description='way to find evaluate similarity of two vectors')
--- mem0/vector_stores/mongodb_atlas.py
+++ mem0/vector_stores/mongodb_atlas.py
@@ -1,10 +1,11 @@
 from mem0.vector_stores.base import VectorStoreBase
 
 from pymongo.mongo_client import MongoClient
+from pymongo.operations import SearchIndexModel
 from pymongo.server_api import ServerApi
 from pymongo.errors import CollectionInvalid
 
-from typing import List
+from typing import List, Dict, Any
 
 import logging
 
@@ -19,6 +20,7 @@ class MongoDBAtlas(VectorStoreBase):
             collection_name: str,
             embedding_model_dims: int,
             vector_field: str,
+            similarity: str,
             client: MongoClient = None,
             uri: str = None,
             database_name: str = None
@@ -31,11 +33,38 @@ class MongoDBAtlas(VectorStoreBase):
                 params['host'] = uri
                 params['server_api'] = ServerApi(version='1') 
             self.client = MongoClient(**params)
+        self._embeding_model_dims = embedding_model_dims
+        self._similarity = similarity
+        self._index_name = "vector-search-index"
         self.collection_name = collection_name
         self.vector_field = vector_field
         self.database = self.client.get_database(database_name)
         self.collection = None
         self.create_col()
+
+    def _create_index(self):
+        idxs = [idx['name'] for idx in self.collection.list_search_indexes().to_list()]
+        if self._index_name in idxs:
+            return
+        search_index_model = SearchIndexModel(
+            definition={
+                "fields": [
+                    {
+                        "type": "vector",
+                        "numDimensions": self._embeding_model_dims,
+                        "path": self.vector_field,
+                        "similarity": self._similarity
+                    }
+                ]
+            },
+            name=self._index_name,
+            type="vectorSearch"
+        )
+        self.collection.create_search_index(model=search_index_model)
+
+    def _update_index(self, filters: Dict[str, Any]):
+        # TODO: get indexed fields from search index and check that all filters are present in the indexed fields
+        pass
 
     def create_col(self):
         try:
@@ -43,6 +72,7 @@ class MongoDBAtlas(VectorStoreBase):
         except CollectionInvalid:
             logger.info("Collection already exists, steps on creating collection on vectore store are skipped")
             self.collection = self.database[self.collection_name]
+        self._create_index()
 
     def insert(self, vectors, payloads=None, ids=None):
         documents = []
@@ -57,19 +87,22 @@ class MongoDBAtlas(VectorStoreBase):
 
 
     def _construct_search_query(self, query, limit, filters):
-        return {
+        return [{
             "$vectorSearch": {
+                "index": self._index_name,
                 "path": self.vector_field,
                 "queryVector": query,
                 "filter": filters,
-                "limit": limit
+                "limit": limit,
+                "numCandidates": limit
             }
-        }
+        }]
 
-    def search(self, query, limit=5, filters=None) -> list:
-        # TODO: test after something is added to the collection
-        result = self.collection.find(filter=self._construct_search_query(query, limit, filters))
-        return []
+    def search(self, query, limit=5, filters={}) -> list:
+        # TODO: fix error with indexing filters for search index
+        self._update_index(filters)
+        result = self.collection.aggregate(self._construct_search_query(query, limit, filters))
+        return result.to_list()
     
     def delete(self, name, vector_id):
         pass
